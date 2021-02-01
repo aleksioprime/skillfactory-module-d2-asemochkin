798a835a1c8fbfa31e53055a410e491e239298ac
aleksioprime <57744848+aleksioprime@users.noreply.github.com>
2021-02-01 22:10:49 +0300

    first commit

---
+++ test_server.py
@@ -0,0 +1,23 @@
+import sentry_sdk
+
+from bottle import Bottle, run
+from sentry_sdk.integrations.bottle import BottleIntegration
+
+sentry_sdk.init(
+    dsn="https://f7a65db35ea04b4fafb93135404a9465@o514688.ingest.sentry.io/5618301",
+    integrations=[BottleIntegration()]
+)
+
+app = Bottle()
+
+@app.route('/success')  
+def success():  
+    return "Hello World!"
+
+@app.route('/fail')  
+def error():  
+    raise RuntimeError("There is an error!")  
+    return  
+  
+  
+app.run(host='localhost', port=8080)
